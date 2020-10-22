cb416b2946badb9ce61e1a74c6b75f65fcf52c71
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-10-22 14:45:14 +0300

    c

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar,Nav, } from 'react-bootstrap';
-
+import { NavDropdown,Nav, } from 'react-bootstrap';
+import {
+MDBNavbar, MDBNavbarBrand,   MDBNavbarToggler, MDBCollapse} from "mdbreact";
 import { Link } from "react-scroll";
 
 
-const Navigation = () => {
+class Navigation extends Component {
+
+  state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
+}
+
+render() {
 	return(
    
-    <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark" >
- 
- <Link
+    <MDBNavbar color="black" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">Navbar</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+    <Link
+    activeClass="active"
+    to="despre-noi"
+    spy={true}
+    smooth={true}
+    offset={-240}
+    duration={300}
+>
+      <Nav.Link href="/Despre">Despre Noi</Nav.Link> </Link>
+       <Link
+    activeClass="active"
+    to="ServiciiID"
+    spy={true}
+    smooth={true}
+    offset={-240}
+    duration={300}>
+      <Nav.Link activeClassName="active" href="/Servicii">Servicii</Nav.Link></Link>
+      <Link
     activeClass="active"
     to="galerie"
     spy={true}
@@ -19,7 +51,22 @@ const Navigation = () => {
     duration={300}
 >      <Nav.Link href="./Galerie/Galerie">Galerie Foto</Nav.Link>
     </Link>
-    <Link
+   
+    <Nav>
+    <NavDropdown title="Preturi" id="collasible-nav-dropdown">
+       <Link
+    activeClass="active"
+    to="interior"
+    spy={true}
+    smooth={true}
+    offset={-240}
+    duration={300}
+>  
+<NavDropdown.Item href="./Preturiinterior">Detaiing Interior</NavDropdown.Item>   
+    </Link> 
+        <NavDropdown.Item href="./Preturi/Preturi.html">Detailing Exterior</NavDropdown.Item>
+      </NavDropdown>
+     <Link
     activeClass="active"
     to="contact"
     spy={true}
@@ -28,9 +75,11 @@ const Navigation = () => {
     duration={10}
 >      <Nav.Link  href="./Contact/Contact"> Contact</Nav.Link>
 </Link>
-</Navbar>
-
-)
+    </Nav>
+     </MDBCollapse>
+      </MDBNavbar>
+);
+}
 }
 
 export default Navigation;
